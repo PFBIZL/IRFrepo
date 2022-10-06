c03960ec11eec5d16353b86e5d6af4c5b9f69871
PFBIZL <rego.szekely@stud.uni-corvinus.hu>
2022-10-06 19:48:12 +0200

    Comm3

--- UserM/UserM/Form1.cs
+++ UserM/UserM/Form1.cs
@@ -29,8 +29,8 @@ namespace UserM
         {
             var u = new User()
             {
-                LastName = textBox1.Text,
-                FirstName = textBox2.Text
+                FullName = textBox1.Text,
+                
             };
             users.Add(u);
         }
