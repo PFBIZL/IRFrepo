e442a082c4ea8312dcfea1ce2b8c1a8a0ef4ce73
PFBIZL <rego.szekely@stud.uni-corvinus.hu>
2022-10-06 19:58:23 +0200

    Comm5

--- UserM/UserM/Resource1.Designer.cs
+++ UserM/UserM/Resource1.Designer.cs
@@ -70,11 +70,11 @@ namespace UserM {
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to .
+        ///   Looks up a localized string similar to Fájlba írás.
         /// </summary>
-        internal static string FirstName {
+        internal static string button2 {
             get {
-                return ResourceManager.GetString("FirstName", resourceCulture);
+                return ResourceManager.GetString("button2", resourceCulture);
             }
         }
         
