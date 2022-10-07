5b9f4efe2269802cb61cc78f4b9cf2bc221b4240
Regő Levente Székely <rego.szekely@stud.uni-corvinus.hu>
2022-10-07 12:14:40 +0200

    4.3Komm

--- 4.het_PFBIZL/4.het_PFBIZL/Form1.cs
+++ 4.het_PFBIZL/4.het_PFBIZL/Form1.cs
@@ -7,6 +7,10 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Reflection;
+using Excel = Microsoft.Office.Interop.Excel;
+
+
 
 namespace _4.het_PFBIZL
 {
