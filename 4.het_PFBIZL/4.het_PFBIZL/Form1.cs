3b117e4d26b4447fb0d5107b00cdfae037967ff1
Regő Levente Székely <rego.szekely@stud.uni-corvinus.hu>
2022-10-07 12:28:03 +0200

    4.4Komm

--- 4.het_PFBIZL/4.het_PFBIZL/Form1.cs
+++ 4.het_PFBIZL/4.het_PFBIZL/Form1.cs
@@ -17,16 +17,96 @@ namespace _4.het_PFBIZL
     public partial class Form1 : Form
     {
         RealEstateEntities context = new RealEstateEntities();
-        List<Flat> Flats;
+        List<Flats> Flats;
+        Excel.Application xlApp;
+        Excel.Workbook xlWB;
+        Excel.Worksheet xlSheet;
 
         public Form1()
         {
             InitializeComponent();
             LoadData();
+             
         }
         private void LoadData()
         {
-            List<Flat> Flats = context.Flat.ToList();
+            List<Flats> Flats = context.Flat.ToList();
+        }
+        private void CreateExcel()
+        {
+            try
+            {
+               
+                xlApp = new Excel.Application();
+
+                
+                xlWB = xlApp.Workbooks.Add(Missing.Value);
+
+               
+                xlSheet = xlWB.ActiveSheet;
+
+               
+                CreateTable(); 
+
+                
+                xlApp.Visible = true;
+                xlApp.UserControl = true;
+            }
+            catch (Exception ex) 
+            {
+                string errMsg = string.Format("Error: {0}\nLine: {1}", ex.Message, ex.Source);
+                MessageBox.Show(errMsg, "Error");
+
+                // Hiba esetén az Excel applikáció bezárása automatikusan
+                xlWB.Close(false, Type.Missing, Type.Missing);
+                xlApp.Quit();
+                xlWB = null;
+                xlApp = null;
+            }
+
+        }
+        private void CreateTable()
+        {
+            string[] Headers = new string[]
+            {
+               "Kód",
+               "Eladó",
+               "Oldal",
+               "Kerület",
+               "Lift",
+               "Szobák száma",
+               "Alapterület (m2)",
+               "Ár (mFt)",
+               "Négyzetméter ár (Ft/m2)"
+
+
+            };
+            object[,] values = new object[Flats.Count, Headers.Length];
+            int counter = 0;
+            foreach (Flats f in Flats)
+            {
+                values[counter, 0] = f.Code;
+                // ...
+                values[counter, 8] = "";
+                counter++;
+            }
+        }
+        private string GetCell(int x, int y)
+        {
+            string ExcelCoordinate = "";
+            int dividend = y;
+            int modulo;
+
+            while (dividend > 0)
+            {
+                modulo = (dividend - 1) % 26;
+                ExcelCoordinate = Convert.ToChar(65 + modulo).ToString() + ExcelCoordinate;
+                dividend = (int)((dividend - modulo) / 26);
+            }
+            ExcelCoordinate += x.ToString();
+
+            return ExcelCoordinate;
+            
         }
     }
 }
