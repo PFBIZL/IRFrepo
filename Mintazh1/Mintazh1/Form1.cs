7aab05ae544e80c1f5645e0e7f7fabd610aa6cd9
PFBIZL <rego.szekely@stud.uni-corvinus.hu>
2022-10-21 11:34:13 +0200

    5het

---
+++ Mintazh1/Mintazh1/Form1.cs
@@ -0,0 +1,110 @@
+﻿using Mintazh1.NewFolder1;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Mintazh1
+{
+    public partial class Form1 : Form
+    {
+        List<OlympicResult> results = new List<OlympicResult>();
+        public Form1()
+        {
+            InitializeComponent();
+            Bem("Summer_olympic_Medals.csv");
+            Evcsin();
+            uj();
+            
+        }
+        private void Bem(string FileName)
+        {
+            using(StreamReader sr = new StreamReader("Summer_olympic_Medals.csv",Encoding.Default))
+            {
+                sr.ReadLine();
+                while (!sr.EndOfStream)
+                {
+                    var line = sr.ReadLine().Split(',');
+                    var or  = new   OlympicResult();
+                    {
+                        or.Year = int.Parse(line[0]);
+                        or.Country = line[3];
+                        var medals = new int[3];
+                        medals[0] = int.Parse(line[5]);
+                        medals[1] = int.Parse(line[6]);
+                        medals[2] = int.Parse(line[7]);
+                        or.Medals = medals;
+                        
+
+
+                        
+                    }
+                }
+            }
+        }
+        private void Evcsin()
+        {
+            var years = (from y in results
+                         orderby y.Year
+                         select y.Year).Distinct();
+            comboBox1.DataSource = years.ToList();
+
+        }
+
+        private int Kim(OlympicResult or)
+        {
+            
+            var betterc = 0;
+            
+            var szures =  from y in results
+                          where y.Year == or.Year && y.Country != or.Country
+                          select y;
+            foreach (var r in szures)
+            {
+                if (r.Medals[0] > or.Medals[0])
+                {
+                    betterc++;
+                }
+                else if (r.Medals[0] == or.Medals[0])
+                {
+                    if (r.Medals[1] > or.Medals[1])
+                    {
+                        betterc++;
+                    }
+                    else if (r.Medals[1] == or.Medals[1])
+                    {
+                        betterc++;
+                    }
+                    if (r.Medals[2] > or.Medals[2])
+                    {
+                        betterc++;
+                    }
+                }
+            }
+
+            return betterc+1;
+
+        }
+
+        private void uj()
+        {
+            foreach (var s in results)
+            {
+                s.Position = Kim(s);
+            }
+
+        }
+
+
+
+
+    }
+
+    
+}
---
+++ gyak1/gyak1/Form1.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace gyak1
+{
+    public partial class Form1 : Form
+    {
+        private List<Sudoku> _list = new List<Sudoku>();
+        private Random rnd = new Random();
+        private Sudoku currentQuiz = null;
+
+        public Form1()
+        {
+
+            
+            InitializeComponent();
+            CreatePlayfield();
+            LoadSudokus();
+            currentQuiz = GetRandomQuiz();
+            NewGame();
+
+        }
+
+        public void CreatePlayfield()
+        {
+            int lineWidth = 5;
+            for (int row = 0; row < 9; row++)
+            {
+                for (int col = 0; col < 9; col++)
+                {
+                    SudokuField sf = new SudokuField();
+                    sf.Left = col * sf.Width + (int)(Math.Floor((double)(col / 3))) * lineWidth;
+                    sf.Top = row * sf.Height + (int)(Math.Floor((double)(row / 3))) * lineWidth;
+                    panel1.Controls.Add(sf);
+
+                }
+
+            }
+
+        }
+        private void LoadSudokus()
+        {
+            _list.Clear();
+            using (StreamReader sr = new StreamReader("sudoku.csv",Encoding.Default))
+            {
+                sr.ReadLine();
+                while (!sr.EndOfStream)
+                {
+                    string[] line = sr.ReadLine().Split(',');
+                    Sudoku s = new Sudoku();
+                    s.Solution = line[0];
+                    s.Quiz = line[1];
+                    _list.Add(s);
+                }
+            }
+
+        }
+
+       private Sudoku GetRandomQuiz()
+        {
+            int rndom = rnd.Next(_list.Count);
+            return _list[rndom];
+
+        }
+        private void NewGame()
+        {
+            currentQuiz = GetRandomQuiz();
+
+        }
+
+        
+    }
+}
