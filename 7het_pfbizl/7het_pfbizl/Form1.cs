a8f45069fbdbb8b8d0ba3c71bacc233096ab8581
PFBIZL <rego.szekely@stud.uni-corvinus.hu>
2022-11-04 12:06:17 +0100

    3

--- 7het_pfbizl/7het_pfbizl/Form1.cs
+++ 7het_pfbizl/7het_pfbizl/Form1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using _7het_pfbizl.Entities;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,9 +13,27 @@ namespace _7het_pfbizl
 {
     public partial class Form1 : Form
     {
+        private List<Ball> _balls = new List<Ball>();
+        private BallFactory _factory;
+        public BallFactory Factory
+        {
+            get { return _factory; }
+            set { _factory = value; }
+        }
         public Form1()
         {
             InitializeComponent();
+            Factory = new BallFactory();
+        }
+
+        private void createTimer_Tick(object sender, EventArgs e)
+        {
+
+        }
+
+        private void conveyorTimer_Tick(object sender, EventArgs e)
+        {
+
         }
     }
 }
