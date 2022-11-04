3f0baa5fc8e9cde5b4af6a12b64537f51b935856
PFBIZL <rego.szekely@stud.uni-corvinus.hu>
2022-11-04 12:07:13 +0100

    4

--- 7het_pfbizl/7het_pfbizl/Form1.cs
+++ 7het_pfbizl/7het_pfbizl/Form1.cs
@@ -28,11 +28,28 @@ namespace _7het_pfbizl
 
         private void createTimer_Tick(object sender, EventArgs e)
         {
-
+            var ball = Factory.CreateNew();
+            _balls.Add(ball);
+            ball.Left = -ball.Width;
+            mainpanel.Controls.Add(ball);
         }
 
         private void conveyorTimer_Tick(object sender, EventArgs e)
         {
+            var maxPosition = 0;
+            foreach (var ball in _balls)
+            {
+                ball.MoveBall();
+                if (ball.Left > maxPosition)
+                    maxPosition = ball.Left;
+            }
+
+            if (maxPosition > 1000)
+            {
+                var oldestBall = _balls[0];
+                mainpanel.Controls.Remove(oldestBall);
+                _balls.Remove(oldestBall);
+            }
 
         }
     }
