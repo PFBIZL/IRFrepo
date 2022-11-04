0101b3aac84e15bfae1b0cd6712ba65c8aa8194b
PFBIZL <rego.szekely@stud.uni-corvinus.hu>
2022-11-04 13:00:04 +0100

    10

--- 7het_pfbizl/7het_pfbizl/Form1.Designer.cs
+++ 7het_pfbizl/7het_pfbizl/Form1.Designer.cs
@@ -32,6 +32,10 @@
             this.mainpanel = new System.Windows.Forms.Panel();
             this.createTimer = new System.Windows.Forms.Timer(this.components);
             this.conveyorTimer = new System.Windows.Forms.Timer(this.components);
+            this.button_ball = new System.Windows.Forms.Button();
+            this.button_car = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // mainpanel
@@ -53,11 +57,54 @@
             this.conveyorTimer.Interval = 10;
             this.conveyorTimer.Tick += new System.EventHandler(this.conveyorTimer_Tick);
             // 
+            // button_ball
+            // 
+            this.button_ball.Location = new System.Drawing.Point(609, 75);
+            this.button_ball.Name = "button_ball";
+            this.button_ball.Size = new System.Drawing.Size(75, 23);
+            this.button_ball.TabIndex = 1;
+            this.button_ball.Text = "Ball";
+            this.button_ball.UseVisualStyleBackColor = true;
+            this.button_ball.Click += new System.EventHandler(this.button_ball_Click);
+            // 
+            // button_car
+            // 
+            this.button_car.Location = new System.Drawing.Point(713, 75);
+            this.button_car.Name = "button_car";
+            this.button_car.Size = new System.Drawing.Size(75, 23);
+            this.button_car.TabIndex = 2;
+            this.button_car.Text = "Car";
+            this.button_car.UseVisualStyleBackColor = true;
+            this.button_car.Click += new System.EventHandler(this.button_car_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(659, 46);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(83, 16);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Coming Next";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.HotTrack;
+            this.button1.Location = new System.Drawing.Point(609, 123);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 4;
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button_car);
+            this.Controls.Add(this.button_ball);
             this.Controls.Add(this.mainpanel);
             this.Name = "Form1";
             this.Text = "Form1";
@@ -71,6 +118,10 @@
         private System.Windows.Forms.Panel mainpanel;
         private System.Windows.Forms.Timer createTimer;
         private System.Windows.Forms.Timer conveyorTimer;
+        private System.Windows.Forms.Button button_ball;
+        private System.Windows.Forms.Button button_car;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
     }
 }
 
--- 7het_pfbizl/7het_pfbizl/Form1.cs
+++ 7het_pfbizl/7het_pfbizl/Form1.cs
@@ -1,4 +1,5 @@
-﻿using _7het_pfbizl.Entities;
+﻿using _7het_pfbizl.Abstractions;
+using _7het_pfbizl.Entities;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -13,12 +14,17 @@ namespace _7het_pfbizl
 {
     public partial class Form1 : Form
     {
-        private List<Ball> _balls = new List<Ball>();
-        private BallFactory _factory;
-        public BallFactory Factory
+        private List<Toy>_toys = new List<Toy>();
+        private IToyFactory _factory;
+        private Toy _nextToy;
+        public IToyFactory Factory
         {
             get { return _factory; }
-            set { _factory = value; }
+            set
+            {
+                _factory = value;
+                DisplayNext();
+            }
         }
 
         public Form1()
@@ -30,7 +36,7 @@ namespace _7het_pfbizl
         private void createTimer_Tick(object sender, EventArgs e)
         {
             var ball = Factory.CreateNew();
-            _balls.Add(ball);
+            _toys.Add(ball);
             ball.Left = -ball.Width;
             mainpanel.Controls.Add(ball);
         }
@@ -38,20 +44,54 @@ namespace _7het_pfbizl
         private void conveyorTimer_Tick(object sender, EventArgs e)
         {
             var maxPosition = 0;
-            foreach (var ball in _balls)
+            foreach (var ball in _toys)
             {
-                ball.MoveBall();
+                ball.MoveToy();
                 if (ball.Left > maxPosition)
                     maxPosition = ball.Left;
             }
 
             if (maxPosition > 1000)
             {
-                var oldestBall = _balls[0];
+                var oldestBall = _toys[0];
                 mainpanel.Controls.Remove(oldestBall);
-                _balls.Remove(oldestBall);
+                _toys.Remove(oldestBall);
             }
 
+        }
+
+        private void button_ball_Click(object sender, EventArgs e)
+        {
+            
+            Factory = new BallFactory();
+
+        }
+
+        private void button_car_Click(object sender, EventArgs e)
+        {
+            Factory = new CarFactory();
+
+        }
+
+        private void DisplayNext()
+        {
+            if (_nextToy != null)
+                Controls.Remove(_nextToy);
+            _nextToy = Factory.CreateNew();
+            _nextToy.Top = label1.Top + label1.Height + 20;
+            _nextToy.Left = label1.Left;
+            Controls.Add(_nextToy);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var button = (Button)sender;
+            var colorPicker = new ColorDialog();
+
+            colorPicker.Color = button.BackColor;
+            if (colorPicker.ShowDialog() != DialogResult.OK)
+                return;
+            button.BackColor = colorPicker.Color;
         }
     }
 }
