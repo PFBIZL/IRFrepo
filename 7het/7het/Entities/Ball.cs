f803612fa94ce022fe71d36f4f295887cd56e3a0
PFBIZL <rego.szekely@stud.uni-corvinus.hu>
2022-11-04 11:46:59 +0100

    1

---
+++ 7het/7het/Entities/Ball.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _7het.Entities
+{
+    internal class Ball : Label
+    {
+        public Ball ()
+        {
+            AutoSize = false;
+            Width = 50;
+            Height = Width;
+            Paint += Ball_Paint;
+        }
+
+        
+
+
+    }
+    
+}
