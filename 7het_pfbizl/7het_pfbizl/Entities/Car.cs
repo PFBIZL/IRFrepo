7718d87f3f2a2b3f3434c597da32f8682d2abbb2
PFBIZL <rego.szekely@stud.uni-corvinus.hu>
2022-11-04 12:31:08 +0100

    6

---
+++ 7het_pfbizl/7het_pfbizl/Entities/Car.cs
@@ -0,0 +1,18 @@
+﻿using _7het_pfbizl.Abstractions;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _7het_pfbizl.Entities
+{
+    internal class Car : Toy
+    {
+        protected override void DrawImage(Graphics g)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
