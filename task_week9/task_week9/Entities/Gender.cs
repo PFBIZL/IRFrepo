5c10e2b6fa915e475465c03c2754c7682d45d24d
PFBIZL <rego.szekely@stud.uni-corvinus.hu>
2022-12-02 12:02:54 +0100

    2

---
+++ task_week9/task_week9/Entities/Gender.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task_week9.Entities
+{
+    public enum  Gender
+    {
+        Male = 1,
+        Female = 2,
+
+    }
+    Gender ádámNeme = Gender.Male;
+    Gender évaNeme = (Gender)2;
+}
