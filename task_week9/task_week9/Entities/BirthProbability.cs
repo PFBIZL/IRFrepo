9261cee75696bef306262d7ba32e4996512540f5
PFBIZL <rego.szekely@stud.uni-corvinus.hu>
2022-12-02 12:08:06 +0100

    3

---
+++ task_week9/task_week9/Entities/BirthProbability.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task_week9.Entities
+{
+    internal class BirthProbability
+    {
+        public int BirthYear { get; set; }
+        public int NbrOfChildren { get; set; }
+        public double P { get; set; }
+    }
+}
---
+++ task_week9/task_week9/Entities/DeathProbability.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task_week9.Entities
+{
+    public class DeathProbability
+    {
+        public int BirthYear { get; set; }
+        public Gender Gender { get; set; }
+        public double P { get; set; }
+    }
+}
